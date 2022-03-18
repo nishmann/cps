46ac64b94eaa0e713be80599743507d144cd0992
nischmann <aziretsuiuntbek@gmail.com>
2022-03-18 20:48:34 +0600

    show/hidden about text

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 import '../scss/style.scss';
 
 // Мобильный меню
-
 let burgerBtn = document.querySelector(".button-item_burger");
 let burgerBtnClose = document.querySelector(".button-item_close");
 let mobileMenuContainer = document.querySelector(".aside");
@@ -13,21 +12,18 @@ if (!mobileMenuContainer.classList.contains("aside_show")) {
     document.getElementById("screen").style.display = "block";
   })
 }
-
 windowBtn.addEventListener("click", () => {
   mobileMenuContainer.classList.remove("aside_show");
   document.getElementById("screen").style.display = "none";
   document.getElementById("call").style.right = "-600px";
   document.getElementById("modal-feedback").style.right = "-600px";
 })
-
 burgerBtnClose.addEventListener("click", () => {
   mobileMenuContainer.classList.remove("aside_show");
   document.getElementById("screen").style.display = "none";
 })
 
 // Заказать звонок и Обратная связь
-
 let callBtn = document.querySelector(".button-item_phone_background");
 let feedbackBtn = document.querySelector(".button-item_chat_background");
 let callBtnHeader = document.getElementById("button-item_phone_background");
@@ -40,19 +36,16 @@ callBtn.addEventListener("click", () => {
   document.getElementById("call").style.transition = "300ms";
   document.getElementById("screen").style.display = "block";
 });
-
 callBtnHeader.addEventListener("click", () => {
   document.getElementById("call").style.right = "0px";
   document.getElementById("call").style.transition = "300ms";
   document.getElementById("screen").style.display = "block";
 });
-
 callBtnClose.addEventListener("click", () => {
   document.getElementById("call").style.right = "-600px";
   document.getElementById("call").style.transition = "300ms";
   document.getElementById("screen").style.display = "none";
 });
-
 callBtnFeedback.addEventListener("click", () => {
   document.getElementById("modal-feedback").style.right = "0px"
   document.getElementById("modal-feedback").style.transition = "300ms"
@@ -70,21 +63,39 @@ feedbackBtnClose.addEventListener("click", () => {
 });
 
 
-// Показать и скрывать брендов
-let toggleBtnServices = document.querySelector(".services__btn");
-let servicesGroup = document.querySelector(".services__group");
+// Показать/Скрывать полный текс about
+let showFullText = document.querySelector(".about__full-text-btn");
+let hiddenText = document.querySelector(".about__text-full");
+let textContainer = document.querySelector(".about__text");
 
-toggleBtnServices.addEventListener("click", () => {
-  toggleBtnServices.classList.toggle("services__btn--show");
-
-  if (toggleBtnServices.classList.contains("services__btn--show")) {
-    toggleBtnServices.textContent = "Скрыть";
-    servicesGroup.classList.add("services__group--full");
+showFullText.addEventListener("click", () => {
+  showFullText.classList.toggle("show-text");
+  textContainer.classList.toggle("about__text-show");
+  if (showFullText.classList.contains("show-text")) {
+    document.getElementById("full-text").style.display = "block";
+    showFullText.textContent = "Скрыть текст";
   } else {
-    toggleBtnServices.textContent = "Показать все";
-    servicesGroup.classList.remove("services__group--full");
+    document.getElementById("full-text").style.display = "none";
+    showFullText.textContent = "Читать далее";
   }
 });
+
+// Показать и скрывать брендов
+// let toggleBtnServices = document.querySelector(".services__btn");
+// let servicesGroup = document.querySelector(".services__group");
+//
+// toggleBtnServices.addEventListener("click", () => {
+//   toggleBtnServices.classList.toggle("services__btn--show");
+//
+//   if (toggleBtnServices.classList.contains("services__btn--show")) {
+//     toggleBtnServices.textContent = "Скрыть";
+//     servicesGroup.classList.add("services__group--full");
+//   } else {
+//     toggleBtnServices.textContent = "Показать все";
+//     servicesGroup.classList.remove("services__group--full");
+//   }
+// });
+
 
 // Swiper
 
